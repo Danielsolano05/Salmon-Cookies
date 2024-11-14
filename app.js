6ba029a5e37cb718bdeeadd6520f1dfe71e9ea4a
daniel solano <Danielsolano05@gmail.com>
2024-11-14 05:40:35 -0800

    update

---
+++ app.js
@@ -0,0 +1,110 @@
+'use strict';
+
+//stores
+
+const seattle = {
+locationName:seattle,
+minCustomersPerHour:12,
+maxCustomersPerHour:25,
+avgCookiesPerSale:8.5,
+cookiesEachHour: [],
+estimate: function () {
+  this.cookiesEachHour = estimateSales(this);
+}
+
+};
+
+const tokyo = {
+locationName:tokyo,
+minCustomersPerHour:15,
+maxCustomersPerHour:30,
+avgCookiesPerSale:6.8,
+cookiesEachHour: [],
+estimate: function () {
+  this.cookiesEachHour = estimateSales(this);
+}
+
+};
+
+const dubai = {
+locationName:dubai,
+minCustomersPerHour:20,
+maxCustomersPerHour:38,
+avgCookiesPerSale:9.5,
+cookiesEachHour: [],
+estimate: function () {
+  this.cookiesEachHour = estimateSales(this);
+}
+
+};
+
+const paris = {
+locationName:paris,
+minCustomersPerHour:35,
+maxCustomersPerHour:48,
+avgCookiesPerSale:8.8,
+cookiesEachHour: [],
+estimate: function () {
+  this.cookiesEachHour = estimateSales(this);
+}
+
+};
+const lima = {
+locationName:lima,
+minCustomersPerHour:10,
+maxCustomersPerHour:22,
+avgCookiesPerSale:9.2,
+cookiesEachHour: [],
+estimate: function () {
+  this.cookiesEachHour = estimateSales(this);
+}
+};
+//application 
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm','7pm'];
+const stores = [seattle, tokyo, dubai, paris, lima];
+function random (min, max) {
+  return Math.floor(math.random() *(max-min + 1))+min;
+}
+
+function estimateSales (store) {
+  const sales =[];
+  for (let i =0; i< hours.length; i++){
+    const numCustomers = randome(store.minCustomersPerHour, maxCustomersPerHour);
+    const hoursSales=match.ceil(numCustomers * store.avgCookiesPerSale);
+    sales.push(hourSales);
+  }
+  return sales;
+}
+
+function render(stores) {
+ 
+  let total = 0;
+  const root = document.getElementsById('root');
+}
+
+
+const location = document.getElement('Section')
+location.classList.add('location')
+root.appendChild(location);
+
+const title = document.createElement('h2');
+title.textContent =store.locationName;
+location.appendChild(title);
+
+
+const list = document.createElement('ul');
+location.appendChild(list);
+
+
+
+for (let i = 0; i < hours.length; i++) {
+  total += store.cookiesEachHour[i];
+  const listItems = document.createElement('li');
+  listItems.textContent =hours[i] + ':' + store.cookiesEachHour[i] + 'cookies';
+  list.appendChild(listItems);
+
+
+  const totalItems = document.createDocumentFragment('li');
+  totalItems.textContent = 'total:' + total + 'cookies';
+  list.appendChild(totalItems)
+}
